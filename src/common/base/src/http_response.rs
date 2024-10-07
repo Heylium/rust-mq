6a1abcd26c19604bd623d8391930060412f32614
Heylium <tuanhsiang@163.com>
2024-10-07 23:24:33 +0800

    placement-center http module added.

---
+++ src/common/base/src/http_response.rs
@@ -0,0 +1,18 @@
+use serde::Serialize;
+
+pub struct Response<T> {
+    pub code: u64,
+    pub data: T,
+}
+
+pub fn success_response<T: Serialize>(data: T) -> String {
+    let resp = Response {
+        code: 0,
+        data,
+    };
+    serde_json::to_string(&resp).unwrap()
+}
+
+pub fn error_response() -> String {
+    "".to_string()
+}
--- src/placement-center/src/server/http/index.rs
+++ src/placement-center/src/server/http/index.rs
@@ -1,6 +1,7 @@
 use axum::extract::State;
 use crate::server::http::server::HttpServerState;
+use common_base::http_response::success_response;
 
 pub async fn index(State(_): State<HttpServerState>) -> String {
-
+    success_response("{}")
 }
